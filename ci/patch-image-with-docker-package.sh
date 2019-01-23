985780146a6fc17beaf1182b63069dfa281eb7c4
Adam Szkoda <adaszko@gmail.com>
2019-01-23 20:58:01 +0100

    Make images patching work with openjdk:11-jre-slim image

--- ci/patch-image-with-docker-package.sh
+++ ci/patch-image-with-docker-package.sh
@@ -4,20 +4,25 @@ set -o nounset
 set -o errexit
 
 main () {
-    if [ $# -lt 2 ]; then
-        echo "$0: wrong number of arguments; expected 2, got $#"
+    if [ $# -lt 3 ]; then
+        echo "$0: wrong number of arguments; expected 3, got $#"
         return 1
     fi
     original_image=$1; shift
     output_image=$1; shift
-    docker_package=docker.io
-    if [ $# -lt 0 ]; then
-        docker_package=$1; shift
-    fi
+    docker_package=$1; shift
 
     container_hash=$(docker run --detach "$original_image")
     docker exec "$container_hash" apt update
-    docker exec "$container_hash" apt install --yes "$docker_package"
+    if [ "$docker_package" = docker.io ]; then
+        docker exec "$container_hash" apt install --yes docker.io
+    else
+        docker exec "$container_hash" apt install --yes apt-transport-https ca-certificates curl software-properties-common curl gnupg
+        docker exec "$container_hash" sh -c 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -'
+        docker exec "$container_hash" add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu    xenial    stable"
+        docker exec "$container_hash" apt update
+        docker exec "$container_hash" apt install --yes docker-ce
+    fi
     docker commit "$container_hash" "$output_image"
     docker stop "$container_hash"
 }
