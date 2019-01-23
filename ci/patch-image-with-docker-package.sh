0bf8531e03057a5844fee26f9a1aa8835fed1bca
Adam Szkoda <adaszko@gmail.com>
2019-01-23 11:09:24 +0100

    Revert "Simplify the solution"
    This reverts commit c5f9c5d2f7d18b7a7af39b122ae1fb79f77d9392.

---
+++ ci/patch-image-with-docker-package.sh
@@ -0,0 +1,20 @@
+#/bin/bash -ue
+
+
+main () {
+    if [[ $# -ne 2 ]]; then
+        echo "$0: wrong number of arguments; expected 2, got $#"
+        return 1
+    fi
+    local original_image=$1; shift
+    local output_image=$1; shift
+
+    local container_hash=$(docker run --detach "$original_image")
+    docker exec "$container_hash" apt update
+    docker exec "$container_hash" apt install --yes docker.io
+    docker commit "$container_hash" "$output_image"
+    docker stop "$container_hash"
+}
+
+
+main "$@"
