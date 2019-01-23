0ba35bbf15c1f6011fc6d634234fff4facba8a9e
Adam Szkoda <adaszko@gmail.com>
2019-01-23 18:10:10 +0100

    Adapt a /bin/bash script to be runnable on /bin/sh

--- ci/patch-image-with-docker-package.sh
+++ ci/patch-image-with-docker-package.sh
@@ -4,14 +4,14 @@ set -o nounset
 set -o errexit
 
 main () {
-    if [[ $# -ne 2 ]]; then
+    if [ $# -ne 2 ]; then
         echo "$0: wrong number of arguments; expected 2, got $#"
         return 1
     fi
-    local original_image=$1; shift
-    local output_image=$1; shift
+    original_image=$1; shift
+    output_image=$1; shift
 
-    local container_hash=$(docker run --detach "$original_image")
+    container_hash=$(docker run --detach "$original_image")
     docker exec "$container_hash" apt update
     docker exec "$container_hash" apt install --yes docker.io
     docker commit "$container_hash" "$output_image"
