12cc0d8a9262455200c91e0708d9a6477550d983
Adam Szkoda <adaszko@gmail.com>
2019-01-29 16:31:03 +0100

    Cleaner command for metrics pushing

--- metrics/loop-metrics.sh
+++ metrics/loop-metrics.sh
@@ -3,8 +3,7 @@ set -axe
 
 while [ ! -f ./stop-loop ]; do
     for instance in bootstrap validator1 validator2 validator3 validator4; do
-        (curl http://$instance:30012/metrics > $instance.txt) || true
-        cat $instance.txt | curl --data-binary @- http://prometheus-pushgateway:9091/metrics/job/${DRONE_COMMIT_SHA}/instance/$instance
+        curl --silent --show-error "http://${instance}:30012/metrics" | curl --silent --show-error --data-binary @- "http://prometheus-pushgateway:9091/metrics/job/${DRONE_COMMIT_SHA}/instance/${instance}"
     done
     sleep 0.5
 done
